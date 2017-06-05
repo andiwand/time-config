13d7b17d1452c45d178a884acfd17f1f560a8431
Andreas Stefl <stefl.andreas@gmail.com>
2017-06-05 23:17:40 +0200

    Update start.py

--- src/timeconfig/start.py
+++ src/timeconfig/start.py
@@ -61,12 +61,15 @@ def parse_config(args):
                     # http://doc.ntp.org/current-stable/drivers/driver22.html
                     ntp_config.append("server 127.127.22.%s minpoll 4 maxpoll 4 iburst true%s" % (unit, prefer))
                     ntp_config.append("fudge 127.127.22.%s stratum %s" % (unit, stratum))
+                    ntp_config.append("fudge 127.127.22.%s flag3 1" % unit) # enable kernel PPS discipline
                 elif driver == "nmea":
                     # http://doc.ntp.org/current-stable/drivers/driver20.html
                     ntp_config.append("server 127.127.20.%s mode 17 minpoll 4 maxpoll 4 iburst true%s" % (unit, prefer))
-                    ntp_config.append("fudge 127.127.20.%s flag1 1" % unit) # enable PPS
-                    ntp_config.append("fudge 127.127.20.%s time2 0.4" % unit) # serial delay
                     ntp_config.append("fudge 127.127.20.%s stratum %s" % (unit, stratum))
+                    # TODO: option to enable pps
+                    ntp_config.append("fudge 127.127.20.%s flag1 1" % unit) # enable PPS
+                    ntp_config.append("fudge 127.127.20.%s flag3 1" % unit) # kernel discipline
+                    ntp_config.append("fudge 127.127.20.%s time2 0.4" % unit) # serial delay
                 else:
                     log("unknown reference clock.")
                     return None
