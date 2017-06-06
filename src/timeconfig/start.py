879ba230c257348dccbd19aea2319ca81054e66b
Andreas Stefl <stefl.andreas@gmail.com>
2017-06-06 21:04:07 +0200

    Update start.py

--- src/timeconfig/start.py
+++ src/timeconfig/start.py
@@ -56,20 +56,20 @@ def parse_config(args):
                 unit = source.find("unit").text if source.find("unit") is not None else "0"
                 if driver == "local":
                     # http://doc.ntp.org/current-stable/drivers/driver1.html
-                    ntp_config.append("server 127.127.1.%s minpoll 4 maxpoll 4%s" % (unit, prefer))
+                    ntp_config.append("server 127.127.1.%s minpoll 4 maxpoll 4 iburst%s" % (unit, prefer))
                     ntp_config.append("fudge 127.127.1.%s stratum %s" % (unit, stratum))
                 elif driver == "pps":
                     # http://doc.ntp.org/current-stable/drivers/driver22.html
-                    ntp_config.append("server 127.127.22.%s minpoll 4 maxpoll 4%s" % (unit, prefer))
+                    ntp_config.append("server 127.127.22.%s minpoll 4 maxpoll 4 iburst%s" % (unit, prefer))
                     ntp_config.append("fudge 127.127.22.%s stratum %s" % (unit, stratum))
-                    #ntp_config.append("fudge 127.127.22.%s flag3 1" % unit) # enable kernel PPS discipline
+                    ntp_config.append("fudge 127.127.22.%s flag3 1" % unit) # enable kernel PPS discipline
                 elif driver == "nmea":
                     # http://doc.ntp.org/current-stable/drivers/driver20.html
-                    ntp_config.append("server 127.127.20.%s mode 17 minpoll 4 maxpoll 4%s" % (unit, prefer))
+                    ntp_config.append("server 127.127.20.%s mode 17 minpoll 4 maxpoll 4 iburst%s" % (unit, prefer))
                     ntp_config.append("fudge 127.127.20.%s stratum %s" % (unit, stratum))
                     # TODO: option to enable pps
-                    #ntp_config.append("fudge 127.127.20.%s flag1 1" % unit) # enable PPS
-                    #ntp_config.append("fudge 127.127.20.%s flag3 1" % unit) # kernel discipline
+                    ntp_config.append("fudge 127.127.20.%s flag1 1" % unit) # enable PPS
+                    ntp_config.append("fudge 127.127.20.%s flag3 1" % unit) # kernel discipline
                     ntp_config.append("fudge 127.127.20.%s time2 0.4" % unit) # serial delay
                 else:
                     log("unknown reference clock.")
